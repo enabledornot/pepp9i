87a962092858a2b0d2a3738ae3dbfb46e51eb126
enabledornot <102371948+enabledornot@users.noreply.github.com>
2022-06-21 23:21:28 -0400

    added to command class

--- pep9col.py
+++ pep9col.py
@@ -6,21 +6,33 @@ def resolveCollisions(code):
     globalVars = []
     usedVars = []
     colCount = 0
+    newCMD = []
     for i in code:
-        split = pep9lib.splitArgs(i)
-        if split[0]==".GLOBAL":
-            globalVars.append(split[1])
+        for ii in i.split("\n"):
+            newCMD.append(command(ii))
+            split = pep9lib.splitArgs(ii)
+            if split[0]==".GLOBAL":
+                globalVars.append(split[1])
 class command:
     def __init__(self,stri):
         self.pointer = pep9lib.getRef(stri)
         self.inst = pep9lib.getInst(stri)
         self.args = pep9lib.getArgs(stri)
         self.com = pep9lib.getComments(stri)
+    def __str__(self):
+        return self.rebuild()
+    def isPoint(self):
+        if self.pointer=="":
+            return False
+        return True
     def rebuild(self):
         newStr = ""
         if self.pointer!="":
-            newStr+=self.pointer + " "
+            newStr+=self.pointer + ": "
         if self.inst!="":
             newStr+=self.inst + " "
-        if self.args!="":
-            newSt+=",".join(self.args)
+        if len(self.args)!=0:
+            newStr+=",".join(self.args) + " "
+        if self.com!="":
+            newStr+=self.com
+        return newStr
